381f7760912bd4f77f251957847146af36e3c7cd
Wiola Janczura <wiola.janczura@wp.pl>
2023-05-19 12:38:00 +0000

    update

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,9 +7,10 @@ const {
   login,
   logout,
   currentUser,
-  setAvatar,
+  updateAvatar,
 } = require(`${basedir}/controllers/auth`);
 const { validateUser } = require("./validation");
+const updateAvatar = require("../../controllers/auth/updateAvatar");
 const ctrlWrapper = require(`${basedir}/helpers/ctrlWrapper`);
 
 router.post("/register", validateUser, ctrlWrapper(register));
@@ -20,6 +21,6 @@ router.get("/logout", auth, ctrlWrapper(logout));
 
 router.get("/current", auth, ctrlWrapper(currentUser));
 
-router.patch("/avatars", auth, upload.single("avatar"), ctrlWrapper(setAvatar));
+router.patch("/avatars", auth, upload.single("avatar"), ctrlWrapper(updateAvatar));
 
 module.exports = router;
